47b00654d3a85f6c3f60441777a39e0480c01083
foguetedoenemadm-glitch <foguetedoenemadm@gmail.com>
2025-08-03 13:21:20 -0300

    Add files via upload

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -1,29 +1,18 @@
-
-"use client"
-
 import * as React from "react"
-import * as LabelPrimitive from "@radix-ui/react-label"
-import { cva, type VariantProps } from "class-variance-authority"
-
+import { Label as RadixLabel } from "@radix-ui/react-label"
 import { cn } from "@/lib/utils"
 
-const labelVariants = cva(
-  "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-)
-
 const Label = React.forwardRef<
-  React.ElementRef<typeof LabelPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> &
-    VariantProps<typeof labelVariants>
+  React.ElementRef<typeof RadixLabel>,
+  React.ComponentPropsWithoutRef<typeof RadixLabel>
 >(({ className, ...props }, ref) => (
-  <LabelPrimitive.Root
+  <RadixLabel
     ref={ref}
-    className={cn(labelVariants(), className)}
+    className={cn("text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70", className)}
     {...props}
   />
 ))
-Label.displayName = LabelPrimitive.Root.displayName
+
+Label.displayName = "Label"
 
 export { Label }
-
-    
